c80210b4b6a9eb3dffd9b9f9e8fbaac4a37ff171
Hossain <hossainrabbi259@gmail.com>
2021-01-26 11:52:44 +0600

    update & calculator deposit and balance

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const loginBtn = document.getElementById('login-btn');
+const addDeposit = document.getElementById('deposit-btn');
 
 // Login button event handler
 loginBtn.addEventListener('click', function () {
@@ -7,3 +8,24 @@ loginBtn.addEventListener('click', function () {
   loginArea.style.display = 'none';
   transactionArea.style.display = 'block';
 });
+
+// Deposit & Balance button event handler
+addDeposit.addEventListener('click', function () {
+  const depositValue = document.getElementById('deposit-value').value;
+  const depositValueNumber = parseFloat(depositValue);
+
+  // Deposit
+  updateAmount('current-deposit', depositValueNumber);
+  // Balance
+  updateAmount('current-balance', depositValueNumber);
+
+  document.getElementById('deposit-value').value = '';
+});
+
+// Deposit & Balance calculator
+function updateAmount(id, depositValueNumber) {
+  const current = document.getElementById(id).innerText;
+  currentNumber = parseFloat(current);
+  const totalAmount = currentNumber + depositValueNumber;
+  document.getElementById(id).innerText = totalAmount;
+}
